c1644ced3e14b2930f56b7bc119be6b0a8257030
Thomas Hunt <thomas.d.hunt@hotmail.co.uk>
2019-11-21 15:35:47 +0000

    Update redditScraper.py

--- redditScraper.py
+++ redditScraper.py
@@ -1,56 +1,32 @@
 import praw
 import csv
 import re
+import reddit_tools
 
-def format_string_list(string_list):
-    new_string_list = []
-    if len(string_list) > 0:
-       
-        for string in string_list:
-            new_string = string.replace(',', '')
-            new_string_list.append(new_string)
-        new_string_list = list(filter(None,new_string_list)) 
-        return new_string_list
-    
-    else: 
-        return string_list    
+tools = reddit_tools
 
 reddit = praw.Reddit(client_id='XVyMfJI3msk0-g',
                     client_secret='ZGGcc0zAimZgQ8oDZwiwx-zogCo',
-                    username='',
-                    password='',
+                    username='AwkwardDragonfruit1',
+                    password='548AJimpGKvRFrF',
                     user_agent='fypscraperv1')
-
 subreddit = reddit.subreddit('depression')
 hot_dep = subreddit.hot(limit=30)
 reddit_data_list = []
+
+
 for submission in hot_dep:
-    
     if not submission.stickied:    
         
         #Split selftext into a list prior assigning to data_list 
         text_body = submission.selftext
-   
-        sentences = re.split('\.|\?|!|\n',text_body)     
-        sentences = format_string_list(sentences)
+        
+        sentences = re.split('\.|\?|!|\n',text_body)
+        sentences = tools.format_string_list(sentences)
         
         reddit_data = {'id':submission.id, 'author':submission.name, 'text':sentences} 
         reddit_data_list.append(reddit_data)
 
-
-
-
-#Getters for reddit data dicts
-def get_id(r_data):
-    return str(r_data['id'])
-
-def get_author(r_data):
-    return str(r_data['author'])
-
-def get_text(r_data):
-    return list(r_data['text'])
-
-#Method for formatting list of strings (removing commas)
 
 #csv writer with formatting
 with open ('redditData.csv', 'w', newline='') as f:
@@ -60,12 +36,10 @@ with open ('redditData.csv', 'w', newline='') as f:
     writer.writeheader()
     for reddit_data in reddit_data_list:
         
-        reddit_id = get_id(reddit_data)
-        reddit_author = get_author(reddit_data)
-        reddit_sentence_list = get_text(reddit_data)
+        reddit_id = tools.get_value_tostring(reddit_data, 'id')
+        reddit_author = tools.get_value_tostring(reddit_data, 'author')
+        reddit_sentence_list = tools.get_value_tolist(reddit_data, 'text')
         
-        #print("#######", reddit_sentence_list[1])
-
         for sentence in reddit_sentence_list:
             writer.writerow({'id': reddit_id, 'author': reddit_author, 'text':sentence} )
 
